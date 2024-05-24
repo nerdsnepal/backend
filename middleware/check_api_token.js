174ac522c0b2b46ac045d5cc0c07c1956217993f
Safal Shrestha <computerstha@gmail.com>
2024-05-24 21:13:28 +0545

    Changed API key

--- middleware/check_api_token.js
+++ middleware/check_api_token.js
@@ -2,11 +2,11 @@ const { isEmpty } = require("../common/utils")
 
 
 const CheckAPIAcessToken = (req,res,next)=>{
-    const token = req.headers['sichu-api-key']
+    const token = req.headers['ecommerce-api-key']
     if(isEmpty(token)){
         return res.status(401).json({success:false,error:"Api key required"})
     }
-    if(token===process.env.SICHU_API_TOKEN){
+    if(token===process.env.PIXSHOP_API_TOKEN){
         return next()
     }
     return res.status(401).json({success:false,error:"Invalid api key"})
